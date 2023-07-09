72abc95e969c23d77b0ecfba2d2088b0c3a941a7
Vasudev Tejam <vasudevtejam@outlook.com>
2023-07-09 19:01:47 +0530

    authController bug fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -31,7 +31,7 @@ const createTokenAndSend = function (user, statusCode, res, req) {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    secure: req.secure || req.headers('x-forwarded-proto') === 'https',
+    secure: req.secure || req.header('x-forwarded-proto') === 'https',
   });
 
   // Remove the password and active status from the output
